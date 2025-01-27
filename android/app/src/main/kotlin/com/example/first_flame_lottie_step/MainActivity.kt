cd54a6cbb3a0750c577d703303d5bb7701771537
MBA <MBA@d.c>
2025-01-27 11:03:39 +0200

    commit

---
+++ android/app/src/main/kotlin/com/example/first_flame_lottie_step/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.first_flame_lottie_step
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flame_lottie/flame_lottie.dart';
+import 'package:flutter/material.dart';
+import 'package:flame/game.dart';
+
+void main() {
+  runApp(GameWidget(game: FlameFirstStep()));
+}
+
+//Перше знайомство з flame_lottie
+class FlameFirstStep extends FlameGame {
+
+  @override
+  Future<void> onLoad() async {
+    final LottieBuilder lottieBuilder = Lottie.asset('assets/win.json');
+    //Компонування анімації, щоб Flame використовував анімації
+    final LottieComposition lottieComposition = await loadLottie(lottieBuilder);
+    //Створення самого компоненту анімації, цей комонент вже можна добавляти до світу, який вам взбреде в голову :)
+    //Також можна тут погратися з position та size на ваш розсуд
+    //Тільки запреЩаю ставити дуже великі значення, бо наш кубок (який зароблено непосильним трудом) полетить в стратосферу :)
+    LottieComponent animation = LottieComponent(lottieComposition,
+        position: Vector2.all(250), size: Vector2.all(200));
+    add(animation);
+  }
+}
